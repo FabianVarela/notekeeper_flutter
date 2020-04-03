e6d46dfb43177c312689367364e74cfb401fd8b0
Fabian Varela Bonett <jfabian.303@gmail.com>
2020-04-03 14:08:51 -0500

    Modify note list

--- lib/screens/note_list.dart
+++ lib/screens/note_list.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:notekeeper_flutter/database/database_helper.dart';
 import 'package:notekeeper_flutter/models/note.dart';
 import 'package:notekeeper_flutter/screens/note_detail.dart';
@@ -7,145 +8,238 @@ import 'package:sqflite/sqflite.dart';
 
 class NoteList extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return NoteListState();
-  }
+  State<StatefulWidget> createState() => NoteListState();
 }
 
 class NoteListState extends State<NoteList> {
-  DatabaseHelper databaseHelper = DatabaseHelper();
-  List<Note> noteList;
-  int count = 0;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  DatabaseHelper _databaseHelper = DatabaseHelper();
+
+  List<Note> _noteList;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _noteList = List<Note>();
+    _updateListView();
+  }
 
   @override
   Widget build(BuildContext context) {
-    if(noteList == null) {
-      noteList = List<Note>();
-      updateListView();
-    }
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My notes'),
+      key: _scaffoldKey,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: <Widget>[
+            _setHeader(),
+            RefreshIndicator(
+              onRefresh: () async => _updateListView(),
+              child: _noteList.isNotEmpty
+                  ? _createNoteList()
+                  : _createEmptyMessage(),
+            ),
+          ],
+        ),
       ),
-      body: getNoteListView(),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Add note',
         child: Icon(Icons.add),
-        onPressed: () {
-          debugPrint('FAB clicked');
-          navigateToDetail(Note('', 2, ''), 'Add note');
-        }
+        onPressed: () => _navigateToDetail(Note('', 2, ''), 'Add note'),
       ),
     );
   }
 
-  ListView getNoteListView() {
-    TextStyle titleStyle = Theme.of(context).textTheme.subhead;
-
-    return ListView.builder(
-      itemCount: count,
-      itemBuilder: (BuildContext context, int position) {
-        return Card(
-          color: Colors.white,
-          elevation: 2,
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundColor: getPriorityColor(noteList[position].priority),
-              child: getPriorityIcon(noteList[position].priority)
+  Widget _setHeader() {
+    return SafeArea(
+      bottom: false,
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        margin: EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Text(
+                'My notes',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w400,
+                  color: Theme.of(context).backgroundColor,
+                ),
+              ),
             ),
-            title: Text(noteList[position].title, style: titleStyle),
-            subtitle: Text(noteList[position].date),
-            trailing: GestureDetector(
-              child: Icon(Icons.delete, color: Colors.grey),
-              onTap: () {
-                _delete(context, noteList[position]);
-              },
-            ),
-            onTap: () {
-              debugPrint("ListTile Tapped");
-             navigateToDetail(noteList[position], 'Edit note');
-            }
-          )
-        );
-      }
+            CircleAvatar(
+              child: Icon(Icons.account_circle, size: 40),
+            )
+          ],
+        ),
+      ),
     );
   }
 
-  Color getPriorityColor(int priority) {
-    switch(priority) {
-      case 1:
-        return Colors.red;
-        break;
-      case 2:
-        return Colors.yellow;
-        break;
-      default:
-        return Colors.yellow;
-    }
+  Widget _createEmptyMessage() {
+    return ListView(
+      shrinkWrap: true,
+      children: <Widget>[
+        Container(
+          height: MediaQuery.of(context).size.height * .7,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 5),
+                child: Icon(
+                  Icons.lightbulb_outline,
+                  size: 150,
+                ),
+              ),
+              Text(
+                'It\'s alone. Add a note',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Theme.of(context).indicatorColor,
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 
-  Icon getPriorityIcon(int priority) {
-    switch(priority) {
-      case 1:
-        return Icon(Icons.play_arrow);
-        break;
-      case 2:
-        return Icon(Icons.keyboard_arrow_right);
-        break;
-      default:
-        return Icon(Icons.keyboard_arrow_right);
-    }
+  Widget _createNoteList() {
+    return Container(
+      height: MediaQuery.of(context).size.height * .85,
+      child: StaggeredGridView.countBuilder(
+        padding: EdgeInsets.zero,
+        crossAxisCount: 4,
+        shrinkWrap: true,
+        itemCount: _noteList.length,
+        itemBuilder: (_, int index) => _createNoteItem(_noteList[index]),
+        staggeredTileBuilder: (int index) => StaggeredTile.fit(2),
+      ),
+    );
   }
 
-  void navigateToDetail(Note note, String title) async {
-    String result = await Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return NoteDetail(note, title);
-    }));
+  Widget _createNoteItem(Note note) {
+    return Card(
+      elevation: 5,
+      color: note.priority == 1 ? Color(0xFFCF5136) : Color(0xFFCFC336),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: GestureDetector(
+        onTap: () => _navigateToDetail(note, 'Edit note'),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 3),
+                child: Text(
+                  note.title,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                child: Text(
+                  note.date,
+                  style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Icon(note.priority == 1
+                      ? Icons.arrow_upward
+                      : Icons.arrow_downward),
+                  GestureDetector(
+                    child: Icon(Icons.delete),
+                    onTap: () => _delete(note),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-    if(result.isNotEmpty) {
+  void _navigateToDetail(Note note, String title) async {
+    final String result = await Navigator.push(
+      context,
+      PageRouteBuilder<String>(
+        pageBuilder: (_, __, ___) => NoteDetail(note, title),
+        transitionsBuilder: (_, Animation<double> anim1, __, Widget child) {
+          final Offset begin = Offset(0.0, 1.0);
+          final Offset end = Offset.zero;
+
+          final Animatable<Offset> tween = Tween<Offset>(begin: begin, end: end)
+              .chain(CurveTween(curve: Curves.ease));
+
+          return SlideTransition(
+            position: anim1.drive(tween),
+            child: child,
+          );
+        },
+      ),
+    );
+
+    if (result.isNotEmpty) {
       _showAlertDialog('Status', result);
-      updateListView();
+      _updateListView();
     }
   }
 
-  void _delete(BuildContext context, Note note) async {
-    int result = await databaseHelper.deleteNote(note.id);
+  void _delete(Note note) async {
+    final int result = await _databaseHelper.deleteNote(note.id);
 
-    if(result != 0) {
-      _showSnackBar(context, 'Note delete successfully');
-      updateListView();
+    if (result != 0) {
+      _showSnackBar('Note delete successfully');
+      _updateListView();
     }
   }
-  
-  void _showSnackBar(BuildContext context, String message) {
-    final snackbar = SnackBar(content: Text(message));
-    Scaffold.of(context).showSnackBar(snackbar);
+
+  void _showSnackBar(String message) {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text(message),
+      duration: Duration(seconds: 3),
+    ));
   }
 
-  void updateListView() {
-    final Future<Database> dbFuture = databaseHelper.initializeDatabase();
+  void _updateListView() {
+    final Future<Database> dbFuture = _databaseHelper.initializeDatabase();
 
-    dbFuture.then((database) {
-      Future<List<Note>> noteListFuture = databaseHelper.getNoteList();
-      noteListFuture.then((noteList) {
-        setState(() {
-          this.noteList = noteList;
-          this.count = noteList.length;
-        });
+    dbFuture.then((Database database) {
+      final Future<List<Note>> noteListFuture = _databaseHelper.getNoteList();
+      noteListFuture.then((List<Note> noteList) {
+        setState(() => this._noteList = noteList);
       });
     });
   }
 
   void _showAlertDialog(String title, String message) {
-    AlertDialog alertDialog = AlertDialog(
-      title: Text(title),
-      content: Text(message)
-    );
-
     showDialog(
       context: context,
-      builder: (_) => alertDialog
+      builder: (_) => AlertDialog(title: Text(title), content: Text(message)),
     );
   }
 }
